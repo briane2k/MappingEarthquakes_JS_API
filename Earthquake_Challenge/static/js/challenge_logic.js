fdd71719b6cc9dd45522a042122fb46a21502ef3
Neags <38869150+Neags@users.noreply.github.com>
2022-03-27 23:48:33 -0700

    Adding Del 3

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -15,15 +15,24 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	accessToken: API_KEY
 });
 
+//My Third tileLayer dark
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
 	center: [40.7, -94.5],
 	zoom: 3,
-	layers: [streets]
+	layers: [dark]
 });
 
 // Create a base layer that holds all three maps.
 let baseMaps = {
+  "Dark": dark,
   "Streets": streets,
   "Satellite": satelliteStreets
 };
@@ -159,4 +168,35 @@ legend.onAdd = function() {
 
     MytectonicL.addTo(map);
   });
+
+
+
+
+
+  // 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
+//  d3.json().then(function(data) {
+
+  // 4. Use the same style as the earthquake data.
+  
+  
+  // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+  
+  
+  // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+  
+  
+  // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+  // sets the style of the circle, and displays the magnitude and location of the earthquake
+  //  after the marker has been created and styled.
+//  L.geoJson(, {
+      
+//  });
+  // 8. Add the major earthquakes layer to the map.
+  
+  // 9. Close the braces and parentheses for the major earthquake data.
+//  });
+
+
+
+
 });
